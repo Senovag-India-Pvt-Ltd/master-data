2d43e52ff3873fe914e5fb16d9dfb894f1d9bdc9
shraddha <shraddhanku2001@gmail.com>
2023-12-26 15:41:29 +0530

    User Master Changes

--- src/main/java/com/sericulture/masterdata/model/api/useMaster/UserMasterResponse.java
+++ src/main/java/com/sericulture/masterdata/model/api/useMaster/UserMasterResponse.java
@@ -66,6 +66,9 @@ public class UserMasterResponse {
     @Schema(name = "designationId", example = "1")
     Long designationId;
 
+    @Schema(name = "name", example = "1")
+    String name;
+
     @Schema(name = "phoneNumber", example = "9988776655")
     String phoneNumber;
 
