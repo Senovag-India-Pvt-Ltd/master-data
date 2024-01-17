8fcd8dd2f8378c45682b74b5dfe470629fd7885f
Ashwika <ashwikakharvi@gmail.com>
2024-01-17 13:40:38 +0530

    Tr Group and Tr Institution Master  CRUD

---
+++ src/main/java/com/sericulture/masterdata/model/api/trGroupMaster/TrGroupMasterResponse.java
@@ -0,0 +1,25 @@
+package com.sericulture.masterdata.model.api.trGroupMaster;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TrGroupMasterResponse {
+    @Schema(name="trGroupMasterId", example = "1")
+    int trGroupMasterId;
+
+    @Schema(name = "trGroupMasterName", example = "Karnataka")
+    String trGroupMasterName;
+
+    @Schema(name = "error", example = "true")
+    Boolean error;
+
+    @Schema(name = "error_description", example = "Username or password is incorrect")
+    String error_description;
+}
---
+++ src/main/java/com/sericulture/masterdata/model/api/trInstitutionMaster/TrInstitutionMasterRequest.java
@@ -0,0 +1,16 @@
+package com.sericulture.masterdata.model.api.trInstitutionMaster;
+
+import com.sericulture.masterdata.model.api.RequestBody;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class TrInstitutionMasterRequest extends RequestBody {
+    @Schema(name = "trInstitutionMasterName", example = "Karnataka", required = true)
+    String trInstitutionMasterName;
+}
