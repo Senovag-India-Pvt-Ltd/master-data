645285be6ef2fd027dc76f5f0200ec207b2ef937
jaibharata <145244644+jaibharata@users.noreply.github.com>
2024-01-19 15:47:03 +0530

    Jwt audit field integration

--- src/main/java/com/sericulture/masterdata/model/entity/BaseEntity.java
+++ src/main/java/com/sericulture/masterdata/model/entity/BaseEntity.java
@@ -1,10 +1,13 @@
 package com.sericulture.masterdata.model.entity;
 
+import com.sericulture.authentication.service.UserInfoDetails;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.*;
+import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.security.Principal;
 import java.util.Date;
 @MappedSuperclass
 @FilterDef(name = "activeEducationFilter", parameters = @ParamDef(name = "active", type = Boolean.class))
@@ -12,11 +15,11 @@ public class BaseEntity {
 
     @Column(name = "CREATED_BY")
     @GenericGenerator(name = "uuid", strategy = "uuid2") //TODO once security is added we need to add the user if from principal
-    private int createdBy;
+    private String createdBy;
 
     @GenericGenerator(name = "uuid", strategy = "uuid2") //TODO once security is added we need to add the user if from principal
     @Column(name = "MODIFIED_BY")
-    private int modifiedBy;
+    private String modifiedBy;
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "CREATED_DATE")
@@ -35,11 +38,16 @@ public class BaseEntity {
     public void prePersist() {
         if(active == null)
             active = true;
+        Principal principal = SecurityContextHolder.getContext().getAuthentication();
+        this.createdBy = principal.getName();
+        this.modifiedBy = principal.getName();
     }
 
     @PreUpdate
     public void preUpdate() {
         if(active == null)
             active = true;
+        Principal principal = SecurityContextHolder.getContext().getAuthentication();
+        this.modifiedBy = principal.getName();
     }
 }
