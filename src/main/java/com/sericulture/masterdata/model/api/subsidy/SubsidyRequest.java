ac642390662583ca215041a06c1623f9867fdc3d
swathivs810 <152892551+swathivs810@users.noreply.github.com>
2024-02-14 16:39:36 +0530

    special character changes

--- src/main/java/com/sericulture/masterdata/model/api/subsidy/SubsidyRequest.java
+++ src/main/java/com/sericulture/masterdata/model/api/subsidy/SubsidyRequest.java
@@ -2,6 +2,7 @@ package com.sericulture.masterdata.model.api.subsidy;
 
 import com.sericulture.masterdata.model.api.RequestBody;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @AllArgsConstructor
@@ -10,9 +11,11 @@ import lombok.*;
 @Setter
 @EqualsAndHashCode
 public class SubsidyRequest extends RequestBody {
+    @Pattern(regexp = "^[a-zA-Z0-9\\s]*$", message = "Subsidy name must contain only letters and numbers")
     @Schema(name = "subsidyName", example = "Subsidy 1", required = true)
     String subsidyName;
 
+    @Pattern(regexp = "^[a-zA-Z0-9\\s\\u0C80-\\u0CFF]*$", message = "Subsidy name in kannada must contain only letters and numbers")
     @Schema(name = "subsidyNameInKannada",  example = "ಭಾಷೆ")
     String subsidyNameInKannada;
 }
--- src/main/java/com/sericulture/masterdata/model/api/workingInstitution/WorkingInstitutionRequest.java
+++ src/main/java/com/sericulture/masterdata/model/api/workingInstitution/WorkingInstitutionRequest.java
@@ -2,6 +2,7 @@ package com.sericulture.masterdata.model.api.workingInstitution;
 
 import com.sericulture.masterdata.model.api.RequestBody;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @AllArgsConstructor
@@ -10,10 +11,11 @@ import lombok.*;
 @NoArgsConstructor
 @EqualsAndHashCode
 public class WorkingInstitutionRequest extends RequestBody {
-
+    @Pattern(regexp = "^[a-zA-Z0-9\\s]*$", message = "Working institution name must contain only letters and numbers")
     @Schema(name = "workingInstitutionName", example = "Karnataka", required = true)
     String workingInstitutionName;
 
+    @Pattern(regexp = "^[a-zA-Z0-9\\s\\u0C80-\\u0CFF]*$", message = "Working institution name in kannada must contain only letters and numbers")
     @Schema(name = "workingInstitutionNameInKannada",  example = "ಭಾಷೆ")
     String workingInstitutionNameInKannada;
 }
