a4de74674942266c2488d1d4cf54709698c13427
swathivs810 <152892551+swathivs810@users.noreply.github.com>
2024-02-14 11:05:26 +0530

    special character changes

--- src/main/java/com/sericulture/masterdata/model/api/designation/DesignationRequest.java
+++ src/main/java/com/sericulture/masterdata/model/api/designation/DesignationRequest.java
@@ -2,6 +2,7 @@ package com.sericulture.masterdata.model.api.designation;
 
 import com.sericulture.masterdata.model.api.RequestBody;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @AllArgsConstructor
@@ -10,9 +11,11 @@ import lombok.*;
 @NoArgsConstructor
 @EqualsAndHashCode
 public class DesignationRequest extends RequestBody {
+    @Pattern(regexp = "^[a-zA-Z0-9\\s]*$", message = "Designation must contain only letters and numbers")
     @Schema(name = "name", example = "Admin", required = true)
     String name;
 
+    @Pattern(regexp = "^[a-zA-Z0-9\\s\\u0C80-\\u0CFF]*$", message = "Designation name in kannada must contain only letters and numbers")
     @Schema(name = "designationNameInKannada",  example = "ಭಾಷೆ")
     String designationNameInKannada;
 }
--- src/main/java/com/sericulture/masterdata/model/api/designation/EditDesignationRequest.java
+++ src/main/java/com/sericulture/masterdata/model/api/designation/EditDesignationRequest.java
@@ -2,6 +2,7 @@ package com.sericulture.masterdata.model.api.designation;
 
 import com.sericulture.masterdata.model.api.RequestBody;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @AllArgsConstructor
@@ -14,9 +15,11 @@ public class EditDesignationRequest extends RequestBody {
     @Schema(name = "designationId", example = "1")
     Integer designationId;
 
+    @Pattern(regexp = "^[a-zA-Z0-9\\s]*$", message = "Designation must contain only letters and numbers")
     @Schema(name = "name", example = "Admin", required = true)
     String name;
 
+    @Pattern(regexp = "^[a-zA-Z0-9\\s\\u0C80-\\u0CFF]*$", message = "Designation in kannada must contain only letters and numbers")
     @Schema(name = "designationNameInKannada",  example = "ಭಾಷೆ")
     String designationNameInKannada;
 
