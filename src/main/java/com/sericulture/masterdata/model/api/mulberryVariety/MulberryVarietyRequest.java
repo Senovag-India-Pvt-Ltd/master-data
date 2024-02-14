441c80d36f23e705fcbb0f16eca91c8305909c01
Ashwika <ashwikakharvi@gmail.com>
2024-02-14 13:45:17 +0530

    PlantationType MulberrySource and MulberryVariety RaceMaster and ReasonBidReject CRUD

--- src/main/java/com/sericulture/masterdata/model/api/mulberryVariety/MulberryVarietyRequest.java
+++ src/main/java/com/sericulture/masterdata/model/api/mulberryVariety/MulberryVarietyRequest.java
@@ -2,6 +2,7 @@ package com.sericulture.masterdata.model.api.mulberryVariety;
 
 import com.sericulture.masterdata.model.api.RequestBody;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @AllArgsConstructor
@@ -11,9 +12,11 @@ import lombok.*;
 @EqualsAndHashCode
 public class MulberryVarietyRequest extends RequestBody {
 
+    @Pattern(regexp = "^[a-zA-Z0-9\\s]*$", message = "MulberryVariety must contain only letters and numbers")
     @Schema(name = "mulberryVarietyName", example = "Mulberry variety 1", required = true)
     String mulberryVarietyName;
 
+    @Pattern(regexp = "^[a-zA-Z0-9\\s\\u0C80-\\u0CFF]*$", message = "MulberryVariety in kannada must contain only letters and numbers")
     @Schema(name = "mulberryVarietyNameInKannada",  example = "ಭಾಷೆ")
     String mulberryVarietyNameInKannada;
 }
