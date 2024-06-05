d1ab61ad80e4331a7f53ee816a609d54c4b569ec
shraddha <shraddhanku2001@gmail.com>
2024-06-05 14:57:22 +0530

    Removed Special Characters

--- src/main/java/com/sericulture/masterdata/model/api/useMaster/EditUserMasterRequest.java
+++ src/main/java/com/sericulture/masterdata/model/api/useMaster/EditUserMasterRequest.java
@@ -15,22 +15,22 @@ public class EditUserMasterRequest extends RequestBody {
     @Schema(name = "userMasterId", example = "1")
     Long userMasterId;
 
-    @Pattern(regexp = "^[a-zA-Z0-9._@ ]*$", message = "First name must contain only letters and numbers")
+//    @Pattern(regexp = "^[a-zA-Z0-9._@ ]*$", message = "First name must contain only letters and numbers")
     @Schema(name = "firstName", example = "Shraddha", required = true)
     String firstName;
 
-    @Pattern(regexp = "^[a-zA-Z0-9._@ ]*$", message = "Middle name must contain only letters and numbers")
+//    @Pattern(regexp = "^[a-zA-Z0-9._@ ]*$", message = "Middle name must contain only letters and numbers")
     @Schema(name = "middleName", example = "Nagaraja", required = true)
     String middleName;
 
-    @Pattern(regexp = "^[a-zA-Z0-9._@ ]*$", message = "Last name must contain only letters and numbers")
+//    @Pattern(regexp = "^[a-zA-Z0-9._@ ]*$", message = "Last name must contain only letters and numbers")
     @Schema(name = "lastName", example = "Kharvi", required = true)
     String lastName;
 
-    @Schema(name = "password", example = "12345", required = true)
+//    @Schema(name = "password", example = "12345", required = true)
     String password;
 
-    @Pattern(regexp = "^[a-zA-Z0-9@.\\s]*$", message = "Email must contain only letters and numbers")
+//    @Pattern(regexp = "^[a-zA-Z0-9@.\\s]*$", message = "Email must contain only letters and numbers")
     @Schema(name = "emailID", example = "12S", required = true)
     String emailID;
 
@@ -49,18 +49,18 @@ public class EditUserMasterRequest extends RequestBody {
     @Schema(name = "marketMasterId", example = "1")
     Long marketMasterId;
 
-    @Pattern(regexp = "^[a-zA-Z0-9._@]*$", message = "User name must contain only letters and numbers")
+//    @Pattern(regexp = "^[a-zA-Z0-9._@]*$", message = "User name must contain only letters and numbers")
     @Schema(name = "username", example = "test")
     String username;
 
     @Schema(name = "designationId", example = "1")
     Long designationId;
 
-    @Pattern(regexp = "^[+0-9\\s]*$", message = "Phone number must contain only numbers")
+//    @Pattern(regexp = "^[+0-9\\s]*$", message = "Phone number must contain only numbers")
     @Schema(name = "phoneNumber", example = "9988776655")
     String phoneNumber;
 
-    @Pattern(regexp = "^[a-zA-Z0-9\\s]*$", message = "ddoCode name must contain only letters and numbers")
+//    @Pattern(regexp = "^[a-zA-Z0-9\\s]*$", message = "ddoCode name must contain only letters and numbers")
     @Schema(name = "ddoCode", example = "ddoCode 1", required = true)
     String ddoCode;
 
@@ -70,7 +70,7 @@ public class EditUserMasterRequest extends RequestBody {
     @Schema(name = "userTypeId", example = "1")
     Long userTypeId;
 
-    @Pattern(regexp = "^[a-zA-Z0-9-\\s]*$", message = "Device Id must contain only letters and numbers")
+//    @Pattern(regexp = "^[a-zA-Z0-9-\\s]*$", message = "Device Id must contain only letters and numbers")
     @Schema(name = "deviceId", example = "998s3gf6478776655")
     String deviceId;
 
--- src/main/java/com/sericulture/masterdata/model/api/useMaster/UserMasterRequest.java
+++ src/main/java/com/sericulture/masterdata/model/api/useMaster/UserMasterRequest.java
@@ -11,22 +11,22 @@ import lombok.*;
 @Setter
 @EqualsAndHashCode
 public class UserMasterRequest extends RequestBody {
-    @Pattern(regexp = "^[a-zA-Z0-9._@ ]*$", message = "First name must contain only letters and numbers")
+//    @Pattern(regexp = "^[a-zA-Z0-9._@ ]*$", message = "First name must contain only letters and numbers")
     @Schema(name = "firstName", example = "Shraddha", required = true)
     String firstName;
 
-    @Pattern(regexp = "^[a-zA-Z0-9._@ ]*$", message = "Last name must contain only letters and numbers")
+//    @Pattern(regexp = "^[a-zA-Z0-9._@ ]*$", message = "Last name must contain only letters and numbers")
     @Schema(name = "middleName", example = "Nagaraja", required = true)
     String middleName;
 
-    @Pattern(regexp = "^[a-zA-Z0-9._@ ]*$", message = "Last name must contain only letters and numbers")
+//    @Pattern(regexp = "^[a-zA-Z0-9._@ ]*$", message = "Last name must contain only letters and numbers")
     @Schema(name = "lastName", example = "Kharvi", required = true)
     String lastName;
 
     @Schema(name = "password", example = "12345", required = true)
     String password;
 
-    @Pattern(regexp = "^[a-zA-Z0-9@.\\s]*$", message = "Email must contain only letters and numbers")
+//    @Pattern(regexp = "^[a-zA-Z0-9@.\\s]*$", message = "Email must contain only letters and numbers")
     @Schema(name = "emailID", example = "12S", required = true)
     String emailID;
 
@@ -45,18 +45,18 @@ public class UserMasterRequest extends RequestBody {
     @Schema(name = "marketMasterId", example = "1")
     Long marketMasterId;
 
-    @Pattern(regexp = "^[a-zA-Z0-9._@]*$", message = "User name must contain only letters and numbers")
+//    @Pattern(regexp = "^[a-zA-Z0-9._@]*$", message = "User name must contain only letters and numbers")
     @Schema(name = "username", example = "test")
     String username;
 
     @Schema(name = "designationId", example = "1")
     Long designationId;
 
-    @Pattern(regexp = "^[0-9+\\s]*$", message = "Phone number must contain only numbers")
+//    @Pattern(regexp = "^[0-9+\\s]*$", message = "Phone number must contain only numbers")
     @Schema(name = "phoneNumber", example = "9988776655")
     String phoneNumber;
 
-    @Pattern(regexp = "^[a-zA-Z0-9\\s]*$", message = "ddoCode name must contain only letters and numbers")
+//    @Pattern(regexp = "^[a-zA-Z0-9\\s]*$", message = "ddoCode name must contain only letters and numbers")
     @Schema(name = "ddoCode", example = "ddoCode 1", required = true)
     String ddoCode;
 
@@ -66,7 +66,7 @@ public class UserMasterRequest extends RequestBody {
     @Schema(name = "userTypeId", example = "1")
     Long userTypeId;
 
-    @Pattern(regexp = "^[a-zA-Z0-9-\\s]*$", message = "Device id must contain only letters and numbers")
+//    @Pattern(regexp = "^[a-zA-Z0-9-\\s]*$", message = "Device id must contain only letters and numbers")
     @Schema(name = "deviceId", example = "998s3gf6478776655")
     String deviceId;
 
