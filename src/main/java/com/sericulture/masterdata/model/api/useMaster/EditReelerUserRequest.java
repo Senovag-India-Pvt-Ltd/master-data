f3ab5d7738b9ef2635ddec6c06cce0c27e0f6baf
shraddha <shraddhanku2001@gmail.com>
2024-06-10 10:55:08 +0530

    removed special character validation

--- src/main/java/com/sericulture/masterdata/model/api/useMaster/EditReelerUserRequest.java
+++ src/main/java/com/sericulture/masterdata/model/api/useMaster/EditReelerUserRequest.java
@@ -17,21 +17,21 @@ public class EditReelerUserRequest extends RequestBody {
     @Schema(name = "reelerId", example = "1", required = true)
     Long reelerId;
 
-    @Pattern(regexp = "^[a-zA-Z0-9._@]*$", message = "User name must contain only letters and numbers")
+//    @Pattern(regexp = "^[a-zA-Z0-9._@]*$", message = "User name must contain only letters and numbers")
     @Schema(name = "username", example = "test")
     String username;
 
     @Schema(name = "password", example = "12345", required = true)
     String password;
 
-    @Pattern(regexp = "^[a-zA-Z0-9@.\\s]*$", message = "Email must contain only letters and numbers")
+//    @Pattern(regexp = "^[a-zA-Z0-9@.\\s]*$", message = "Email must contain only letters and numbers")
     @Schema(name = "emailID", example = "12S", required = true)
     String emailID;
 
     @Schema(name = "roleId", example = "1", required = true)
     Long roleId;
 
-    @Pattern(regexp = "^[+0-9\\s]*$", message = "Phone number must contain only letters and numbers")
+//    @Pattern(regexp = "^[+0-9\\s]*$", message = "Phone number must contain only letters and numbers")
     @Schema(name = "phoneNumber", example = "9988776655")
     String phoneNumber;
 
@@ -41,7 +41,7 @@ public class EditReelerUserRequest extends RequestBody {
     @Schema(name = "designationId", example = "1")
     Long designationId;
 
-    @Pattern(regexp = "^[a-zA-Z0-9-\\s]*$", message = "Device id must contain only letters and numbers")
+//    @Pattern(regexp = "^[a-zA-Z0-9-\\s]*$", message = "Device id must contain only letters and numbers")
     @Schema(name = "deviceId", example = "998s3gf6478776655")
     String deviceId;
 
